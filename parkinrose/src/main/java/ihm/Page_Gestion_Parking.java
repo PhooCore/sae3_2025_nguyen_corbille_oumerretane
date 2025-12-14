1620ab239b087a6911138f09cd8a501fb832b003
oumerretane <emmy.oumerretane@etu.iut-tlse3.fr>
2025-12-14 06:11:55 +0100

    gestion parking admin

---
+++ parkinrose/src/main/java/ihm/Page_Gestion_Parking.java
@@ -0,0 +1,41 @@
+package ihm;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Page_Gestion_Parking extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Page_Gestion_Parking frame = new Page_Gestion_Parking();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Page_Gestion_Parking() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
