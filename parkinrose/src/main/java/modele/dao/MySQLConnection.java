b4494c4f7b0fbb6668d4ae73568e098db214b3d6
pho29 <tuyet.nguyen@etu.iut-tlse3.fr>
2026-02-03 13:22:06 +0100

    Update MySQLConnection.java

--- parkinrose/src/main/java/modele/dao/MySQLConnection.java
+++ parkinrose/src/main/java/modele/dao/MySQLConnection.java
@@ -6,17 +6,29 @@ import java.sql.SQLException;
 
 public class MySQLConnection {
 
-    private static final String URL = "jdbc:mysql://127.0.0.1/parkinrosebdd";
-    private static final String USER = "root";
-    private static final String PASSWORD = "$iutinfo";
+    private static final String HOST = "sql7.freesqldatabase.com";
+    private static final String DATABASE = "sql7816083";
+    private static final String PORT = "3306";
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
+    private static final String USER = "sql7816083";
+    private static final String PASSWORD = "JvQpL3zIAj";
 
     public static Connection getConnection() throws SQLException {
         try {
+            // Pour MySQL 8.0+
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new SQLException("Driver MySQL non trouvé", e);
+            try {
+                // Pour les anciennes versions de MySQL
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+                throw new SQLException("Driver MySQL non trouvé", ex);
+            }
         }
 
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        // Ajout de paramètres de connexion pour meilleure compatibilité
+        String connectionURL = URL + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        
+        return DriverManager.getConnection(connectionURL, USER, PASSWORD);
     }
 }
